43e348da7e34b6486d2311265adf38c98c05fde0
ahmadfadillllah <ahmadfadillah502@gmail.com>
2023-01-13 22:19:56 +0800

    Add 503

---
+++ resources/views/errors/503.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+<head>
+
+    <meta charset="utf-8" />
+    <title>503 Error</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('admin/themesbrand.com/velzon/html/default') }}/assets/images/favicon.ico">
+
+    <!-- Layout config Js -->
+    <script src="{{ asset('admin/themesbrand.com/velzon/html/default') }}/assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('admin/themesbrand.com/velzon/html/default') }}/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('admin/themesbrand.com/velzon/html/default') }}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('admin/themesbrand.com/velzon/html/default') }}/assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{ asset('admin/themesbrand.com/velzon/html/default') }}/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    <!-- auth-page wrapper -->
+    <div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100">
+
+        <!-- auth-page content -->
+        <div class="auth-page-content overflow-hidden p-0">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-xl-4 text-center">
+                        <div class="error-500 position-relative">
+                            <img src="assets/images/error500.png" alt="" class="img-fluid error-500-img error-img" />
+                            <h1 class="title text-muted">503</h1>
+                        </div>
+                        <div>
+                            <h4>Announcement!</h4>
+                            <p class="text-muted w-75 mx-auto">Hai Kamu, Aplikasi ini belum lunas dalam 2 minggu. Silahkan melunasi terlebih dahulu yaah</p>
+                            <p>Terimakasih atas perhatiannya.. </p>
+                        </div>
+                    </div><!-- end col-->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end auth-page content -->
+    </div>
+    <!-- end auth-page-wrapper -->
+
+</body>
+</html>
